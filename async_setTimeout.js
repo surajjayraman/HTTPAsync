69090d3a58a276e4020578aed2661e2fb8408a28
surajjayraman <suraj.jayraman@gmail.com>
2022-10-28 22:10:49 -0400

    Add setTimeout starter code.

---
+++ async_setTimeout.js
@@ -0,0 +1,18 @@
+const craftingCompleteWait = 750
+const combiningMaterialsWait = 250
+const smeltingIronBarsWait = 0
+const shapingIronWait = 500
+
+// Don't touch below this line
+
+setTimeout(() => console.log('Iron Longsword Complete!'), craftingCompleteWait)
+setTimeout(() => console.log('Combining Materials...'), combiningMaterialsWait)
+setTimeout(() => console.log('Smelting Iron Bars...'), smeltingIronBarsWait)
+setTimeout(() => console.log('Shaping Iron...'), shapingIronWait)
+
+console.log('Firing up the forge...')
+
+await sleep(2500)
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}

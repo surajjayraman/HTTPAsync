7b4763f599d924812429b98209a7d37f10bc76de
surajjayraman <suraj.jayraman@gmail.com>
2022-10-30 09:35:13 -0400

    Add custom error statement.

--- errors.js
+++ errors.js
@@ -7,23 +7,23 @@ try {
 }
 
 try {
-    printCharacterStats(4)
-    printCharacterStats('ten')
-    printCharacterStats(10)
+  printCharacterStats(4);
+  printCharacterStats('ten');
+  printCharacterStats(10);
     
-  } catch (err) {
-    console.log(`An error was thrown: ${err}`);
-    // the code cleanly logs:
-    // "An error was thrown: ReferenceError: car is not defined"
+} catch (err) {
+  console.log(`An error was thrown: ${err}`);
+  // the code cleanly logs:
+  // "An error was thrown: Parameter is not a number!"
+}
+  
+  
+// don't touch below this line
+  
+function printCharacterStats(level) {
+  if (isNaN(level)) {
+    throw 'Parameter is not a number!';
   }
-  
-  
-  // don't touch below this line
-  
-  function printCharacterStats(level) {
-    if (isNaN(level)) {
-      throw 'Parameter is not a number!'
-    }
-    console.log(`Your character is level ${level}!`)
-  }
+  console.log(`Your character is level ${level}!`);
+}
   

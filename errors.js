68fce49cc323546f47eed489c33b322709add50c
surajjayraman <suraj.jayraman@gmail.com>
2022-10-30 09:32:00 -0400

    Add code to handle error.

---
+++ errors.js
@@ -0,0 +1,29 @@
+try {
+  const speed = car.speed;
+} catch (err) {
+  console.log(`An error was thrown: ${err}`);
+  // the code cleanly logs:
+  // "An error was thrown: ReferenceError: car is not defined"
+}
+
+try {
+    printCharacterStats(4)
+    printCharacterStats('ten')
+    printCharacterStats(10)
+    
+  } catch (err) {
+    console.log(`An error was thrown: ${err}`);
+    // the code cleanly logs:
+    // "An error was thrown: ReferenceError: car is not defined"
+  }
+  
+  
+  // don't touch below this line
+  
+  function printCharacterStats(level) {
+    if (isNaN(level)) {
+      throw 'Parameter is not a number!'
+    }
+    console.log(`Your character is level ${level}!`)
+  }
+  

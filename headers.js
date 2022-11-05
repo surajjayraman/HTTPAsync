b5c2b7b93fcfa9c9c96ed353c54b445df8101f69
surajjayraman <suraj.jayraman@gmail.com>
2022-11-04 20:18:40 -0400

    Update location object using header X-API-Key.

--- headers.js
+++ headers.js
@@ -1,4 +1,14 @@
 import fetch from "node-fetch";
+
+const generatedApiKey = generateKey();
+const url = 'https://api.boot.dev/v1/courses_rest_api/learn-http/locations/52fdfc07-2182-454f-963f-5f0f9a621d72'
+const newLocationData = {
+  'discovered': false,
+  'id': '52fdfc07-2182-454f-963f-5f0f9a621d72',
+  'name': 'Bloodstone Swamp',
+  'recommendedLevel': 10
+}
+
 function logContentType(resp) {
     // ?
     console.log(resp.headers.get("content-type"));
@@ -7,7 +17,7 @@ function logContentType(resp) {
   
   // don't touch below this line
   
-  const apiKey = generateKey()
+  const apiKey = generateKey();
   const bootdevAPIDomain = 'api.boot.dev'
   
   const items = await getItemData(bootdevAPIDomain)
@@ -34,4 +44,44 @@ function logContentType(resp) {
     }
     return result
   }
-  
+
+// update location object using header X-API-Key
+const oldLocation = await getLocationResponse(generatedApiKey, url)
+console.log(`Got old location: {name: ${oldLocation.name}, recommendedLevel: ${oldLocation.recommendedLevel}}'`)
+
+console.log('Updating location...')
+await putLocation(generatedApiKey, url, newLocationData)
+console.log('Location updated!')
+
+const newGeneratedApiKey = generateKey()
+const newLocation = await getLocationResponse(generatedApiKey, url)
+console.log(`Got new location: {name: ${newLocation.name}, recommendedLevel: ${newLocation.recommendedLevel}}'`)
+
+
+// don't touch below this line
+
+async function getLocationResponse(apiKey, url) {
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'X-API-Key': apiKey,
+      'Content-Type': 'application/json'
+    }
+  })
+  return response.json()
+}
+
+async function putLocation(apiKey, url, data) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'X-API-Key': apiKey,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  return response.json()
+}
+

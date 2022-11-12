fb4389a570fc565cb8196c778b3eea5cf6a9c88f
surajjayraman <suraj.jayraman@gmail.com>
2022-11-12 18:21:21 -0500

    Test JSON.parse on locationString.

---
+++ parsertest.js
@@ -0,0 +1,43 @@
+const json = '{"title": "Avengers Endgame", "Rating":4.7, "inTheaters":false}';
+const obj = JSON.parse(json);
+
+console.log(obj.title);
+// Avengers Endgame
+
+const parseLocation = (locationString) => {
+  // ensure JSON format
+  try {
+    const parsedLocation = JSON.parse(locationString);
+    printLocationObj(parsedLocation);
+  } catch (error) {
+    console.error("invalid json string");
+  }
+};
+  
+// don't touch below this line
+const printLocationObj = (parsed) => {
+  console.log(`id: ${parsed.id}`);
+  console.log(`discovered: ${parsed.discovered}`);
+  console.log(`name: ${parsed.name}`);
+  console.log(`recommendedLevel: ${parsed.recommendedLevel}`);
+};
+  
+parseLocation(`
+  {
+      "discovered": false,
+      "id": "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+      "name": "Bandit Camp",
+      "recommendedLevel": 14
+  `);
+  
+console.log('---');
+  
+parseLocation(`
+  {
+      "discovered": false,
+      "id": "0194fdc2-fa2f-4cc0-81d3-ff12045b73c8",
+      "name": "Bandit Camp",
+      "recommendedLevel": 14
+  }
+  `);
+  
